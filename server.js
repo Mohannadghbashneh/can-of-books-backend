8b8893cba31b71c8673ce854a965d51daafa1ed0
mohannadghbashneh <ghbashneh99@gmail.com>
2022-09-07 00:40:14 +0300

    13

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 // process.env.MONGODP ||process.env.MONGOCLOUD
 
 
-mongoose.connect('mongodb+srv://booksShop:1JO3d3x3qz3RbtJV@mohannad.pw0y3qx.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true}); // 1 - connect mongoose with DB
+mongoose.connect('mongodb+srv://booksshop:zAvbMMC3rWjWV2Bm@cluster0.vgldg9x.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true}); // 1 - connect mongoose with DB
 
 const BooksSchema = new mongoose.Schema({
   title:String,
@@ -33,76 +33,69 @@ const Book = mongoose.model('Book', BooksSchema);
 
 
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-  app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  
-  
-  
-  app.get('/books',BooksHandler)
-  app.post('/addbook',NewBookHandler)
-  app.delete('/deletebook/:id',deleteCatHandler);
-  
-  
-  async function deleteCatHandler(req,res) {
-    const bookid= req.params.id;
-  
-    Book.deleteOne({_id:bookid},(err,result)=>{
-      
-      
-      
-      Book.find({},(err,result)=>{
-  
-        if (err){
-          console.log(err)
-        }
-        else{
-          res.send(result);
-          
-        };
-      })
-      
-    }
-      
-      )
+
+
+app.get('/books',BooksHandler)
+app.post('/addbook',NewBookHandler)
+app.delete('/deletebook/:id',deleteCatHandler);
+app.put('/updatebook/:id',UpdateBookHandler)
+
+
+async function UpdateBookHandler(req,res){
+
+  const bookId=req.params.id;
+  console.log(bookId)
+  const updatedData=req.body;
+  Book.findByIdAndUpdate(updatedData.id,{
+    title:updatedData.title,
+    description:updatedData.description
+
+  },(err,result)=>{
     
-  
-  }
-  
-  
-  async function NewBookHandler(req,res){
-  
-      const receivedData =req.body;
-      
-      await Book.create({
-  
-        title:receivedData.title,
-        description:receivedData.description,
-        status:receivedData.status
-      })
-  
+    if (err) {
+
+      console.log(err)
+    }  
+
+    else{
+
       Book.find({},(err,result)=>{
-  
+
         if (err){
+
           console.log(err)
         }
         else{
-          res.send(result);
-          
-        };
+
+          res.send(result)
+        }
+
       })
+    }
+  })
+
+
   
-    };
+
   
-  
-  
-  function BooksHandler(req,res){
-  
-  
-  
-     Book.find({},(err,result)=>{
-  
+
+
+
+}
+
+function deleteCatHandler(req,res) {
+  const bookid= req.params.id;
+
+  Book.deleteOne({_id:bookid},(err,result)=>{
+    
+    
+    
+    Book.find({},(err,result)=>{
+
       if (err){
         console.log(err)
       }
@@ -110,13 +103,62 @@ const Book = mongoose.model('Book', BooksSchema);
         res.send(result);
         
       };
-   
-  })
-  
+    })
+    
   }
+    
+    )
   
-  
-  
-  
-  
-  app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+}
+
+
+async function NewBookHandler(req,res){
+
+    const receivedData =req.body;
+    
+    await Book.create({
+
+      title:receivedData.title,
+      description:receivedData.description,
+      status:receivedData.status
+    })
+
+    Book.find({},(err,result)=>{
+
+      if (err){
+        console.log(err)
+      }
+      else{
+        res.send(result);
+        
+      };
+    })
+
+  };
+
+
+
+function BooksHandler(req,res){
+
+
+
+   Book.find({},(err,result)=>{
+
+    if (err){
+      console.log(err)
+    }
+    else{
+      res.send(result);
+      
+    };
+ 
+})
+
+}
+
+
+
+
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
